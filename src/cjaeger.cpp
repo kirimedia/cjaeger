67ccf732307de2855730cc439666a67db488f0ca
Alexander Drozdov <aleksandr.drozdov@corp.mail.ru>
2021-12-14 19:52:10 +0300

    allow cjaeger.h to be used with C++ code

--- src/cjaeger.cpp
+++ src/cjaeger.cpp
@@ -1,3 +1,4 @@
+#include "cjaeger.h"
 #include <jaegertracing/Tracer.h>
 
 namespace cjaeger {
--- src/cjaeger.h
+++ src/cjaeger.h
@@ -1,11 +1,21 @@
 #ifndef CJAEGER_H
 #define CJAEGER_H
 
+#include <stddef.h>
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
 void *cjaeger_tracer_create(const char *service_name, const char *agent_addr);
 void cjaeger_tracer_destroy(void *tracer);
 void *cjaeger_span_start(void *tracer, void *parent, const char *operation_name);
 void cjaeger_span_finish(void *span);
 void cjaeger_span_log(void *span, const char *key, const char *value);
 void cjaeger_span_log2(void *span, const char *key, const char *value, size_t value_len);
+
+# ifdef  __cplusplus
+}
+# endif
 
 #endif
