e6ffa1a7bd64a0bd581c2db248872019cefab458
Alexander Drozdov <aleksandr.drozdov@corp.mail.ru>
2021-12-17 11:09:07 +0300

    do not pass C++ exceptions to C code

--- src/cjaeger.cpp
+++ src/cjaeger.cpp
@@ -19,69 +19,89 @@ typedef Wrapper<std::unique_ptr<opentracing::Span> > Span;
 
 extern "C" void *cjaeger_tracer_create(const char *service_name, const char *agent_addr) {
 
-	auto config = jaegertracing::Config(
-		false,
-		false,
-		jaegertracing::samplers::Config(
-			"const",
-			1,
-			"",
-			0,
-			std::chrono::seconds(0)
-		),
-		jaegertracing::reporters::Config(
-			0,
-			std::chrono::seconds(0),
-			true,
-			agent_addr,
-			""
-		),
-		jaegertracing::propagation::HeadersConfig(),
-		jaegertracing::baggage::RestrictionsConfig(),
-		service_name,
-		std::vector<jaegertracing::Tag>(),
-		jaegertracing::propagation::Format::JAEGER
-	);
+	try {
+		auto config = jaegertracing::Config(
+			false,
+			false,
+			jaegertracing::samplers::Config(
+				"const",
+				1,
+				"",
+				0,
+				std::chrono::seconds(0)
+			),
+			jaegertracing::reporters::Config(
+				0,
+				std::chrono::seconds(0),
+				true,
+				agent_addr,
+				""
+			),
+			jaegertracing::propagation::HeadersConfig(),
+			jaegertracing::baggage::RestrictionsConfig(),
+			service_name,
+			std::vector<jaegertracing::Tag>(),
+			jaegertracing::propagation::Format::JAEGER
+		);
 
-	auto tracer = jaegertracing::Tracer::make(config);
+		auto tracer = jaegertracing::Tracer::make(config);
 
-	return new Tracer(tracer);
+		return new Tracer(tracer);
+	} catch (...) {
+		return NULL;
+	}
 }
 
 extern "C" void cjaeger_tracer_destroy(void *tracer) {
 	Tracer *_tracer = (Tracer*)tracer;
-	_tracer->get()->Close();
+	try {
+		_tracer->get()->Close();
+	} catch (...) {
+	}
 	delete _tracer;
 }
 
 extern "C" void *cjaeger_span_start(void *tracer, void *parent, const char *operation_name) {
-	opentracing::StartSpanOptions options;
-	if (parent) {
-		Span *_parent = (Span*)parent;
-		opentracing::ChildOf(&_parent->get()->context()).Apply(options);
+	try {
+		opentracing::StartSpanOptions options;
+		if (parent) {
+			Span *_parent = (Span*)parent;
+			opentracing::ChildOf(&_parent->get()->context()).Apply(options);
+		}
+		Tracer *_tracer = (Tracer*)tracer;
+		auto span = _tracer->get()->StartSpanWithOptions(operation_name, options);
+		return new Span(span);
+	} catch (...) {
+		return NULL;
 	}
-	Tracer *_tracer = (Tracer*)tracer;
-	auto span = _tracer->get()->StartSpanWithOptions(operation_name, options);
-	return new Span(span);
 }
 
 extern "C" void cjaeger_span_finish(void *span) {
 	Span *_span = (Span*)span;
-	_span->get()->Finish();
+	try {
+		_span->get()->Finish();
+	} catch (...) {
+	}
 	delete _span;
 }
 
 
 extern "C" void cjaeger_span_log(void *span, const char *key, const char *value) {
 	Span *_span = (Span*)span;
-	std::string _value(value);
-	_span->get()->Log({{key, _value}});
+	try {
+		std::string _value(value);
+		_span->get()->Log({{key, _value}});
+	} catch (...) {
+	}
 }
 
 extern "C" void cjaeger_span_log2(void *span, const char *key, const char *value, size_t value_len) {
 	Span *_span = (Span*)span;
-	std::string _value(value, value + value_len);
-	_span->get()->Log({{key, _value}});
+	try {
+		std::string _value(value, value + value_len);
+		_span->get()->Log({{key, _value}});
+	} catch (...) {
+	}
 }
 
 }
